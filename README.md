3a50619b3f26ae0fbf54e51e3ea9d15ce2defecf
Abhijeet Kumar <abkumarofficial@gmail.com>
2021-06-08 11:47:09 -0700

    Added navbar with scrollToView functionality

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 * [Instructions](#instructions)
 
 ## Instructions
+1: You can open the page in the browser using index.html
+2: By clicking on the various button on the nav bar, you can reach to any section
 
 The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
 
--- js/app.js
+++ js/app.js
@@ -12,46 +12,23 @@
  * JS Standard: ESlint
  * 
 */
+const allSections = document.querySelectorAll('section');
+const navbar = document.getElementById('navbar__list');
+const fragment = document.createDocumentFragment();
 
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+for (let i = 1; i <= allSections.length ; i += 1) {
+    const liData = document.createElement('li');
+    // Adding link to the li and class into it
+    liData.innerHTML = `<a id="navbar-${allSections[i-1].id}" class="menu__link">Section ${i}</a>`
+    liData.addEventListener('click', () => {
+        allSections[i-1].scrollIntoView({
+          behavior: 'smooth',
+          block: 'end',
+          inline: 'nearest',
+        });
+      });
+    fragment.appendChild(liData);
+}
+navbar.appendChild(fragment);
 
 
